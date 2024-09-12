24acde7d021e11cd86054679af8eefcec6d5b5c1
jsjohn100481 <jsjohn@umd.edu>
2024-09-11 20:36:21 -0400

    search function implement ENPM611

--- search.py
+++ search.py
@@ -1,7 +1,15 @@
 import json
 
 def search_json(json_data, search_string):
+    
     results = []
+    for x in json_data:
+        #print(len(x))
+        if(x["User"] == search_string):
+            results.append(x)
+            print ("success")
+
+                
     # Place your search code here
     # you will have to loop through the json_data file you created earlier
     # finally you can store the match in the result list and return it
